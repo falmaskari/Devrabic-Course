8f593cb5c99be4f447088e8a7f5c02d187b8a782
Fatma <f.almaskari113@gmail.com>
2024-04-04 20:31:01 +0400

    JS-Basics

---
+++ Javascript/JS-Basics/script.js
@@ -0,0 +1,106 @@
+let price = 10;
+price -= 20;
+price = price++;
+price = price--;
+let name = "Devrabic";
+let isValid = false;
+let students = ["mike", "youssef", "zein"];
+let data1 = undefined;
+let data2 = null;
+let user = {
+  id: 0,
+  username: "Mike",
+  password: "123",
+};
+let user1 = {
+  id: 0,
+  username: "Mike",
+  password: "123",
+};
+
+// console.log(price);
+// console.log(1 + 2);
+// console.log(1 * 2);
+// console.log(1 - 2);
+// console.log(1 / 2);
+// console.log("1" + "2");
+// console.log(user + user1);
+
+// console.log(1 == 1);
+// console.log(1 != 1);
+// console.log(1 === "1");
+
+// console.log(!true);
+// console.log(true && false);
+// console.log(1 == 1 && 1 == 1);
+// console.log(true || false);
+// console.log(1 == 1 || 1 == 2);
+
+// let check = true;
+// check ? console.log("YES") : console.log("NO");
+
+// if (1 >= 1) console.log("YES");
+// else console.log("NO");
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+
+// while (i <= 50) {
+//   console.log(i);
+//   i++;
+// }
+
+// for (let i = 0; i <= 50; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 0; i <= 50; i++) {
+//   console.log(i);
+// }
+
+// let i = 1;
+
+// switch (i) {
+//   case 0:
+//     console.log("i is 0");
+//     break;
+//   case 1:
+//     console.log("i is 1");
+//     break;
+//   default:
+//     console.log("i is not valid");
+// }
+
+// try {
+//   const a = 1;
+//   a = 2;
+// } catch (err) {
+//   console.error(err);
+//   console.log("error has been found");
+// }
+
+// const a = 1;
+// const b = 2;
+// const c = 5;
+
+// function addNumbers(number1, number2) {
+//   if (typeof number1 == "number" && typeof number2 == "number")
+//     return number1 + number2;
+//   else throw TypeError("Error in parameters");
+// }
+
+// const res = addNumbers("b", "c");
+// console.log(res);
+
+// function example() {
+//   var x = 1;
+//   if (true) {
+//     var x = 2;
+//     console.log(x);
+//   }
+//   console.log(x);
+// }
+
+// example();
